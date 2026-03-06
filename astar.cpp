99f0cf7a562089694bd9b494a5330742207ffe84
katakamrupendra <katakamrupendra@gmail.com>
2026-03-06 18:44:37 +0530

    now i got the neighbours coordinate

--- astar.cpp
+++ astar.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+int map_width = 5;
+int map_height = 5;
+// 0 is free space, 1 is a wall. 
+// We are building a wall right down the middle (x=2).
+vector<int> grid = {
+    0, 0, 1, 0, 0,
+    0, 0, 1, 0, 0,
+    0, 0, 1, 0, 0,
+    0, 0, 0, 0, 0, // A gap in the wall!
+    0, 0, 1, 0, 0
+};
+
 // The Blueprint (Define this outside main)
 struct Node {
     int x;
@@ -47,20 +59,46 @@ int main() {
     open_list.push_back(node_a);
     open_list.push_back(node_b);
 
+    vector<Node> closed_list;
+
     float min = numeric_limits<float>::max(); // Initialize min to the maximum possible float value
     int x = -1;
-    int y = -1;
     for (int i = 0; i < open_list.size(); i++){
 
         if (open_list[i].f < min){
             min = open_list[i].f;
-            x = open_list[i].x;
-            y = open_list[i].y;
-            
+            x = i;  
+                 
         }
     }
+    
+    Node current_node = open_list[x];
+    closed_list.push_back(current_node);
+    open_list.erase(open_list.begin() + x);
     cout << "Cost of the node with the lowest f is " << min << endl;
-    cout << "coordinates of the node with the lowest f is " << x << ", " << y << endl;
+    cout << "coordinates of the node with the lowest f is " << current_node.x << ", " << current_node.y << endl;
+
+     
+    // Loop through the Y offsets (-1, 0, 1)
+    for (int dy = -1; dy <= 1; dy++) {
+        // Loop through the X offsets (-1, 0, 1)
+        for (int dx = -1; dx <= 1; dx++) {
+            
+            // 1. Calculate the neighbor's actual coordinates
+            int neighbor_x = current_node.x + dx;
+            int neighbor_y = current_node.y + dy;
+
+            // 2. Skip the node itself (where dx is 0 AND dy is 0)
+            if (dx == 0 && dy == 0) {
+                continue; // This skips to the next iteration of the loop
+            }
+
+            // 3. The Guardrail: Check if the neighbor is inside the map boundaries!
+            if ( neighbor_x >= 0 && neighbor_x < map_width && neighbor_y >= 0 && neighbor_y < map_height ) {
+                cout << "Valid neighbor found at: " << neighbor_x << ", " << neighbor_y << endl;
+            }
+        }
+    }
 
     return 0;
 }
